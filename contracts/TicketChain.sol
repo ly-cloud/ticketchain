9b7bbfcaf5a35b1e0809564ebe8e68f12cd7b371
Cloud <cloud.ly@outlook.com>
2020-03-17 17:48:39 +0800

    Update mapping designs
    EventTicket
    - constructor calls TicketChain for new eventId
    - use mapping for ticketId => Ticket struct
    - use ticketIdCounter
    - massMint function
    - remove refund, balanceOf
    
    TicketChain
    - map eventId => EventTicket contract
    
    LoyaltyCoin
    - commented out for now

--- contracts/TicketChain.sol
+++ contracts/TicketChain.sol
@@ -1,126 +1,135 @@
 pragma solidity ^0.5.0;
 
+import './EventTicket.sol';
+import './LoyaltyCoin.sol';
+
 contract TicketChain {
-    // for marketplace
+    address OWNER;
+    LoyaltyCoin loyaltyCoin;
+    uint commission;
+    uint rate;
+    uint redeemRate;
+    uint eventIdCounter;
 
-  LoyaltyCoin loyaltyCoin;
-  EventTicket eventTicket;
-  uint256 commission;
-  uint256 rate;
-  uint256 redeemRate;
-  address _owner;
-  
-  constructor(LoyaltyCoin loyaltyCoinAddr, EventTicket eventTicketAddr) public {
-    loyaltyCoin = loyaltyCoinAddr;
-    eventTicket = eventTicketAddr;
-    _owner = msg.sender;
-  }
+    mapping(uint => EventTicket) events;
 
-  struct TicketInfo {
-    uint256 eventId;
-    uint256 ticketId;
-    uint256 price;
-  }
-
-  TicketInfo[] ticketInfos;
-
-  mapping (uint256 => bool) private transactions;
-
-  modifier onlyOwner() {
-    require(_owner == msg.sender);
-    _;
-  }
-
-  modifier onlySeller(uint256 ticketId) {
-    require(eventTicket.ownerOf(ticketId) == msg.sender);
-    _;
-  }
-
-  modifier onlyPrevOwner(uint256 ticketId) {
-    require(eventTicket.getPrevOwner(ticketId) == msg.sender);
-    _;
-  }
-
-  function listTicket(uint256 eventId, uint256 ticketId, uint256 price) puclic onlySeller(ticketId) {
-  	require(price <= eventTicket.getOriginalPrice(ticketId));
-
-    TicketInfo memory newTicketInfo = TicketInfo(eventId, ticketId, price);
-    ticketInfos[ticketId] = newTicketInfo;
-  }
-
-  function changePrice(uint256 ticketId, uint256 newPrice) puclic onlyPrevOwner(ticketId) {
-  	require(eventTicket.ownerOf(ticketId) == _owner);
-  	require(newPrice <= eventTicket.getOriginalPrice(ticketId));
-
-    ticketInfos[ticketId].price = newPrice;
-  }
-
-  function unlistTicket(uint256 ticketId) puclic onlyPrevOwner(ticketId) {
-  	require(eventTicket.ownerOf(ticketId) == _owner);
-
-  	eventTicket.transferTo(ticketId, msg.sender);
-    ticketInfos[ticketId] = 0;
-  }
-
-  function buyTicket(uint256 ticketId, uint256 loyalty) public payable {
-    require(ticketInfos[ticketId] != 0); //is listed
-    require(block.timestamp >= eventTicket.getOpenSaleTime(ticketId) && block.timestamp < eventTicket.getClosingSaleTime(ticketId));
-
-    if(loyalty = 0){
-      require(msg.value >= (ticketInfos[ticketId].price + comission)); //offerred price meets minimum ask
-
-      address payable recipient = address(uint160(eventTicket.ownerOf(ticketId)));
-      recipient.transfer(msg.value - comission);    //transfer (price-comission) to real owner
-
-      emit Transaction(nextTransactionId, ticektId);
-      nextTransactionId++;
-
-    } else {
-      uint256 remaining = ticketInfos[ticketId].price + comission - loyalty/redeemRate;
-      require(msg.value >= remaining);
-
-      address payable recipient = address(uint160(eventTicket.ownerOf(ticketId)));
-      recipient.transfer(remaining);    //transfer remaining amount 
-
-      //TODO from ticketChain to receipent
-      //recipient.transfer(ticketInfos[ticketId].price - remaining {from: _owner});
+    constructor(LoyaltyCoin lcAddress) public {
+        loyaltyCoin = lcAddress;
+        OWNER = msg.sender;
     }
 
-    eventTicket.transferTo(ticketId, msg.sender);
-    loyaltyCoin.issuePoint(msg.sender, rate);
-  }
+    struct TicketInfo {
+        uint eventId;
+        uint ticketId;
+        uint price;
+    }
 
-  function getTicketInfo(uint256 ticketId) public view returns(ticketInfo) {
-    return ticketInfos[ticketId];
-  }
+    TicketInfo[] ticketInfos;
 
-  function getRedeemRate() public view returns(uint256) {
-    return redeemRate;
-  }
+    mapping(uint => bool) private transactions;
 
-  function setRedeemRate(uint256 conversion) public onlyOwner {
-    redeemRate = conversion;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == OWNER);
+        _;
+    }
 
-  function getRate() public view returns(uint256) {
-    return rate;
-  }
+    modifier onlySeller(uint ticketId) {
+        require(eventTicket.ownerOf(ticketId) == msg.sender);
+        _;
+    }
 
-  function setRate(uint256 amount) public onlyOwner {
-    rate = amount;
-  }
+    modifier onlyPrevOwner(uint ticketId) {
+        require(eventTicket.getPrevOwner(ticketId) == msg.sender);
+        _;
+    }
 
-  function getCommission() public view returns(uint256) {
-    return commission;
-  }
+    function newEvent() public returns(uint) {
+        EventTicket etContract = EventTicket(msg.sender);
+        require(etContract.isEventTicket());
+        eventIdCounter++;
+        events[eventIdCounter] = etContract;
+        return eventIdCounter;
+    }
 
-  function setCommission(uint256 value) public onlyOwner {
-    commission = value;
-  }
+    function listTicket(uint256 eventId, uint256 ticketId, uint256 price) public onlySeller(ticketId) {
+        require(price <= eventTicket.getOriginalPrice(ticketId));
 
-  function withdraw() public {
-    if(msg.sender == _owner)
-      msg.sender.transfer(address(this).balance);
-  }
-  
+        TicketInfo memory newTicketInfo = TicketInfo(eventId, ticketId, price);
+        ticketInfos[ticketId] = newTicketInfo;
+    }
+
+    function changePrice(uint256 ticketId, uint256 newPrice) puclic onlyPrevOwner(ticketId) {
+        require(eventTicket.ownerOf(ticketId) == _owner);
+        require(newPrice <= eventTicket.getOriginalPrice(ticketId));
+
+        ticketInfos[ticketId].price = newPrice;
+    }
+
+    function unlistTicket(uint256 ticketId) puclic onlyPrevOwner(ticketId) {
+        require(eventTicket.ownerOf(ticketId) == _owner);
+
+        eventTicket.transferTo(ticketId, msg.sender);
+        ticketInfos[ticketId] = 0;
+    }
+
+    function buyTicket(uint256 ticketId, uint256 loyalty) public payable {
+        require(ticketInfos[ticketId] != 0); //is listed
+        require(block.timestamp >= eventTicket.getOpenSaleTime(ticketId) && block.timestamp < eventTicket.getClosingSaleTime(ticketId));
+
+        if(loyalty = 0){
+            require(msg.value >= (ticketInfos[ticketId].price + comission)); //offerred price meets minimum ask
+
+            address payable recipient = address(uint160(eventTicket.ownerOf(ticketId)));
+            recipient.transfer(msg.value - comission);    //transfer (price-comission) to real owner
+
+            emit Transaction(nextTransactionId, ticektId);
+            nextTransactionId++;
+
+        } else {
+            uint256 remaining = ticketInfos[ticketId].price + comission - loyalty/redeemRate;
+            require(msg.value >= remaining);
+
+            address payable recipient = address(uint160(eventTicket.ownerOf(ticketId)));
+            recipient.transfer(remaining);    //transfer remaining amount
+
+            //TODO from ticketChain to receipent
+            //recipient.transfer(ticketInfos[ticketId].price - remaining {from: _owner});
+        }
+
+        eventTicket.transferTo(ticketId, msg.sender);
+        loyaltyCoin.issuePoint(msg.sender, rate);
+    }
+
+    function getTicketInfo(uint256 ticketId) public view returns(ticketInfo) {
+        return ticketInfos[ticketId];
+    }
+
+    function getRedeemRate() public view returns(uint256) {
+        return redeemRate;
+    }
+
+    function setRedeemRate(uint256 conversion) public onlyOwner {
+        redeemRate = conversion;
+    }
+
+    function getRate() public view returns(uint256) {
+        return rate;
+    }
+
+    function setRate(uint256 amount) public onlyOwner {
+        rate = amount;
+    }
+
+    function getCommission() public view returns(uint256) {
+        return commission;
+    }
+
+    function setCommission(uint256 value) public onlyOwner {
+        commission = value;
+    }
+
+    function withdraw() public {
+        if(msg.sender == _owner)
+        msg.sender.transfer(address(this).balance);
+    }
 }
