b2ffec44c2a17f13ad31da035680a1c2e7cf5681
Cloud <cloud.ly@outlook.com>
2020-03-25 16:10:26 +0800

    Add functions to fetch ticket info from frontend
    - refactor eventName, eventDateTime, venue, openSaleTime, 
    closingSaleTime from struct to EventTicket properties
    - all the above properties will be set in constructor, can be updated by 
    onlyOwner before tickets are listed
    - add getTicket function which returns Ticket struct properties as []
    - add getListing function in market, returns Listing struct as []
    - fix some syntax to follow solidity conventions

--- contracts/TicketChain.sol
+++ contracts/TicketChain.sol
@@ -6,17 +6,11 @@ import './EventTicket.sol';
 contract TicketChain {
     address OWNER;
     // LoyaltyCoin loyaltyCoin;
-    uint commission;
-    uint rate;
-    uint redeemRate;
+    uint public commission;
+    // uint rate;
+    // uint redeemRate;
     uint eventIdCounter;
-
     mapping(uint => EventTicket) events;
-
-    constructor() public {
-        // loyaltyCoin = lcAddress;
-        OWNER = msg.sender;
-    }
 
     struct Listing {
     	address seller;
@@ -26,6 +20,10 @@ contract TicketChain {
 
     mapping(uint => mapping(uint => Listing)) private ticketsListing;
 
+    constructor() public {
+        // loyaltyCoin = lcAddress;
+        OWNER = msg.sender;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == OWNER);
@@ -44,7 +42,15 @@ contract TicketChain {
         _;
     }
 
-    function newEvent() public returns(uint) {
+    function withdraw() public onlyOwner {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    function setCommission(uint _commission) public onlyOwner {
+        commission = _commission;
+    }
+
+    function newEvent() public returns (uint eventId) {
         EventTicket etContract = EventTicket(msg.sender);
         require(etContract.isEventTicket());
         eventIdCounter++;
@@ -52,11 +58,11 @@ contract TicketChain {
         return eventIdCounter;
     }
 
-    function list(
-        uint eventId,
-        uint ticketId,
-        uint price
-    ) public validEvent(eventId) onlyTicketOwner(eventId, ticketId) {
+    function list(uint eventId, uint ticketId, uint price)
+        public
+        validEvent(eventId)
+        onlyTicketOwner(eventId, ticketId)
+    {
         require(!ticketsListing[eventId][ticketId].listed);
         EventTicket eventTicket = events[eventId];
         require(price <= eventTicket.getOriginalPrice(ticketId));
@@ -65,11 +71,11 @@ contract TicketChain {
         ticketsListing[eventId][ticketId] = newListing;
     }
 
-    function updatePrice(
-        uint eventId,
-        uint ticketId,
-        uint newPrice
-    ) public validEvent(eventId) onlyTicketOwner(eventId, ticketId) {
+    function updatePrice(uint eventId, uint ticketId, uint newPrice)
+        public
+        validEvent(eventId)
+        onlyTicketOwner(eventId, ticketId)
+    {
         require(ticketsListing[eventId][ticketId].listed);
         EventTicket eventTicket = events[eventId];
         require(newPrice <= eventTicket.getOriginalPrice(ticketId));
@@ -77,26 +83,27 @@ contract TicketChain {
         ticketsListing[eventId][ticketId].price = newPrice;
     }
 
-    function unlist(
-        uint eventId,
-        uint ticketId
-    ) public validEvent(eventId) onlyTicketOwner(eventId, ticketId) {
+    function unlist(uint eventId, uint ticketId)
+        public
+        validEvent(eventId)
+        onlyTicketOwner(eventId, ticketId)
+    {
         EventTicket eventTicket = events[eventId];
         delete ticketsListing[eventId][ticketId];
         eventTicket.transferTo(ticketId, msg.sender);
     }
 
-    function buy(
-        uint eventId,
-        uint ticketId
-        // uint loyalty
-    ) public payable validEvent(eventId) {
+    function buy(uint eventId, uint ticketId)
+        public
+        payable
+        validEvent(eventId)
+    {
         require(ticketsListing[eventId][ticketId].listed);
         EventTicket eventTicket = events[eventId];
         require(msg.value >= ticketsListing[eventId][ticketId].price);
         require(
-            block.timestamp >= eventTicket.getOpenSaleTime(ticketId)
-            && block.timestamp < eventTicket.getClosingSaleTime(ticketId)
+            block.timestamp >= eventTicket.openSaleTime()
+            && block.timestamp < eventTicket.closingSaleTime()
         );
 
         address payable recipient = address(
@@ -106,39 +113,36 @@ contract TicketChain {
         recipient.transfer(msg.value - commission);
     }
 
-    function getListingPrice(
-        uint eventId,
-        uint ticketId
-    ) public view validEvent(eventId) returns(uint) {
+    function getListing(uint eventId, uint ticketId)
+        public
+        view
+        validEvent(eventId)
+        returns (address seller, uint price)
+    {
         require(ticketsListing[eventId][ticketId].listed);
-        return ticketsListing[eventId][ticketId].price;
+        return (
+            ticketsListing[eventId][ticketId].seller,
+            ticketsListing[eventId][ticketId].price
+        );
     }
 
-    function getRedeemRate() public view returns(uint) {
-        return redeemRate;
+    function getTotalEvents() public view returns (uint) {
+        return eventIdCounter;
     }
 
-    function setRedeemRate(uint conversion) public onlyOwner {
-        redeemRate = conversion;
-    }
-
-    function getRate() public view returns(uint) {
-        return rate;
-    }
-
-    function setRate(uint amount) public onlyOwner {
-        rate = amount;
-    }
-
-    function getCommission() public view returns(uint) {
-        return commission;
-    }
-
-    function setCommission(uint value) public onlyOwner {
-        commission = value;
-    }
-
-    function withdraw() public onlyOwner {
-        msg.sender.transfer(address(this).balance);
-    }
+    // function getRedeemRate() public view returns (uint) {
+    //     return redeemRate;
+    // }
+    //
+    // function setRedeemRate(uint conversion) public onlyOwner {
+    //     redeemRate = conversion;
+    // }
+    //
+    // function getRate() public view returns (uint) {
+    //     return rate;
+    // }
+    //
+    // function setRate(uint amount) public onlyOwner {
+    //     rate = amount;
+    // }
 }
