950198f804b14eb43b8367ed868f87df123c2750
jiahuipuah <e0053053@u.nus.edu>
2020-04-07 11:50:15 +0800

    Update Homepage
    - Use material-ui-image instead of CardMedia for Card image

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,7 +17,7 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+// import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -183,10 +183,14 @@ export function HomePage(props) {
             {events.map(event => (
               <Grid item key={event.eventId} x1s={12} sm={6} md={4}>
                 <Card className={classes.card}>
-                  <CardMedia
+                  {/* <CardMedia
                     className={classes.cardMedia}
                     image="https://source.unsplash.com/random"
                     title={event.eventName}
+                  /> */}
+                  <Image
+                    src="https://source.unsplash.com/random"
+                    aspectRatio={16 / 9}
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
