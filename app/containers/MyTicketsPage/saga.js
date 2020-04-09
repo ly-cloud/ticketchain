49d24e6175b4c2a3507257199b30de6eb74eec10
Edward <wardzxzxc@gmail.com>
2020-04-10 02:52:48 +0800

    WIP
    - tried to get event contract via mapping of events in ticketChain

--- app/containers/MyTicketsPage/saga.js
+++ app/containers/MyTicketsPage/saga.js
@@ -1,16 +1,27 @@
-import { takeLatest, call, put, cps } from 'redux-saga/effects';
-import Web3 from 'web3';
-import { loadedTickets } from './actions';
+import { takeLatest } from 'redux-saga/effects';
+// import Web3 from 'web3';
+// import { loadedTickets } from './actions';
 import { LOAD_TICKETS } from './constants';
-import * as api from '../../utils/apiManager';
+// import * as api from '../../utils/apiManager';
+// import TicketChain from '../../../build/contracts/TicketChain.json';
 // Individual exports for testing
 
-const web3 = new Web3(window.ethereum);
+// const web3 = new Web3(window.ethereum);
 
 export function* loadAllTicketsOwned() {
-  const ownerAddress = yield cps(web3.eth.getCoinbase);
-  const tickets = yield call(api.getMyTickets, ownerAddress);
-  yield put(loadedTickets(tickets.data.tickets));
+  // const ownerAddress = yield cps(web3.eth.getCoinbase);
+  // const tickets = yield call(api.getMyTickets, ownerAddress);
+  // const networkId = yield cps(web3.eth.net.getId);
+  // const networkData = TicketChain.networks[networkId];
+  // const ticketChainAddress = networkData.address;
+  // const ticketChainInstance = new web3.eth.Contract(TicketChain.abi, ticketChainAddress);
+  // const eventInstance = yield ticketChainInstance.events.call(0);
+  // console.log(eventInstance)
+  // for(ticket of tickets) {
+  //   const eventId = ticket.eventId;
+  //   console.log()
+  // }
+  // yield put(loadedTickets(tickets.data.tickets));
 }
 
 export default function* myTicketsPageSaga() {
